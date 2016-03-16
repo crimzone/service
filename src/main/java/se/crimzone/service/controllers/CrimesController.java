4cea3e6be37d13bbe6ed81082d789be30a6cd3b8
Gustav Karlsson <gustav.karlsson@gmail.com>
2016-03-16 11:35:35 +0100

    Added maven download of swagger.yaml

---
+++ src/main/java/se/crimzone/service/controllers/CrimesController.java
@@ -0,0 +1,16 @@
+package se.crimzone.service.controllers;
+
+import io.swagger.inflector.models.RequestContext;
+import io.swagger.inflector.models.ResponseContext;
+import org.joda.time.DateTime;
+
+public class CrimesController {
+
+	public ResponseContext find(RequestContext request, String query, DateTime minTime, DateTime maxTime) {
+		throw new UnsupportedOperationException("Not yet implemented"); // TODO: Implement
+	}
+
+	public ResponseContext findById(RequestContext request, Long id) {
+		throw new UnsupportedOperationException("Not yet implemented"); // TODO: Implement
+	}
+}
